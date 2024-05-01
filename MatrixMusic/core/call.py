86c695169403bd4aed999b4c0c167dd921e2f3af
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2024-05-02 01:57:31 +0300

    Update call.py

--- MatrixMusic/core/call.py
+++ MatrixMusic/core/call.py
@@ -11,8 +11,8 @@ from pytgcalls.exceptions import (
     
 )
 from pytgcalls.types import Update
-from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
+from pytgcalls.types.stream import AudioPiped, AudioVideoPiped
+from pytgcalls.types.stream.quality import HighQualityAudio, MediumQualityVideo
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
